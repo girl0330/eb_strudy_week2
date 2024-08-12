e10f4d387dce97ddd5750062ea3dff2a66e0708e
현경 <girls0330@naver.com>
2024-08-12 12:30:03 +0900

    first commit

---
+++ src/main/java/com/sb/sbweek3/Sbweek3Application.java
@@ -0,0 +1,13 @@
+package com.sb.sbweek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbweek3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbweek3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sb/sbweek3/Sbweek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.sbweek3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbweek3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
